364c7e7669326e56e0abaace9f97ec12697f5cca
Jiarun Wei <63835218+jiarunw@users.noreply.github.com>
2022-03-01 02:14:44 -0500

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Safe AI Scout'
+copyright = '2022, Safe AI Lab'
+author = ['Jinchi Zhang', 'Jiayin Xia', 'Jiarun Wei']
 
 release = '0.1'
 version = '0.1.0'
